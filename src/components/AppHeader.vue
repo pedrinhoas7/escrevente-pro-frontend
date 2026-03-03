8ca44899b73de28939329698058717c4e1e32d94
Pedro <pedro.moraes@vsoft.com.br>
2026-03-03 19:48:35 -0300

    fix: authStore

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import { useAuthStore } from '../stores/auth'
 
 const route = useRoute()
 const router = useRouter()
